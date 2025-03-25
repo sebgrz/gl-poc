3b18223b2f007bf98d75258561cd1e0fb5bea9b1
Seba G <sebag.git@gmail.com>
2025-03-25 18:59:02 +0100

    draw contours

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -7,5 +7,17 @@ out vec4 fragColor;
 uniform sampler2D tex;
 
 void main() {
-	fragColor = texture(tex, texCoord);
+	float TEX_SEGMENT = 1.0 / textureSize(tex, 1).x;
+	vec4 rightTex = texture(tex, vec2(texCoord.x + TEX_SEGMENT * 2.0, texCoord.y));
+	vec4 leftTex = texture(tex, vec2(texCoord.x - TEX_SEGMENT * 2.0, texCoord.y));
+	vec4 topTex = texture(tex, vec2(texCoord.x, texCoord.y - TEX_SEGMENT * 2.0));
+  vec4 texVec = texture(tex, texCoord);
+	vec3 keyColor = vec3(0.0);
+	if ( texVec.rgb == keyColor && (rightTex.rgb != keyColor || leftTex.rgb != keyColor || topTex.rgb != keyColor)) {
+		fragColor = vec4(1.0);
+	} else if (texVec.rgb == keyColor) {
+		discard;
+	} else {
+		fragColor = texVec;
+	}
 }
